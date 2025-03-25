d6e448f676f9d512d6e0d8fd7fe1e68ecb8e5446
jarombustillo <jarombustillo222@gmail.com>
2025-03-25 18:48:56 +0800

    Adding CRUD Endpoints

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ import userRoutes from "./users/user.controller";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware for parsing JSON requests
 app.use(express.json());
